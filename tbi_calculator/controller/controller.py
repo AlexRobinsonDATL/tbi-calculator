b873a61c00c0b1b9151d040e6e8b2149d408373c
AlexRobinsonDATL <alexander.robinson@dunlopatl.co.uk>
2021-04-22 18:17:58 +0100

    :sparkles: add basic controller

--- tbi_calculator/controller/controller.py
+++ tbi_calculator/controller/controller.py
@@ -1,6 +1,28 @@
+from threading import Thread
+from time import sleep
+
+from ..view import View
 from .base import ControllerBase
 
 
 class Controller(ControllerBase):
-    def start(self):
-        raise NotImplementedError
+    def __init__(self, view: View, model=None):
+        self.view = view
+
+    def start(self) -> None:
+        self.view.setup(self)
+        self.view.start_main_loop()
+
+    def execute(self) -> None:
+        thread = Thread(target=self.logic_thread)
+        thread.start()
+
+    def logic_thread(self) -> None:
+        self.view.status = "Running!"
+        email = self.view.email
+        password = self.view.password
+        api_key = self.view.api_key
+
+        sleep(5)
+        print(email, password, api_key)
+        self.view.status = "Ready!"
