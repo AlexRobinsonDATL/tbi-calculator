644be1749e749f5c65d10618611704b865465de8
AlexRobinsonDATL <alexander.robinson@dunlopatl.co.uk>
2021-04-23 11:46:34 +0100

    :construction: integrate app

--- tbi_calculator/__main__.py
+++ tbi_calculator/__main__.py
@@ -1,5 +1,6 @@
 from .controller import Controller
+from .model import SSMetabaseModel
 from .view import TkView
 
-app = Controller(view=TkView(), model=None)
+app = Controller(view=TkView(), model=SSMetabaseModel())
 app.start()
--- tbi_calculator/controller/controller.py
+++ tbi_calculator/controller/controller.py
@@ -1,28 +1,38 @@
 from threading import Thread
-from time import sleep
 
+from ..model.base import Model
 from ..view import View
 from .base import ControllerBase
 
 
 class Controller(ControllerBase):
-    def __init__(self, view: View, model=None):
+    def __init__(self, view: View, model: Model):
         self.view = view
+        self.model = model
 
     def start(self) -> None:
         self.view.setup(self)
         self.view.start_main_loop()
 
     def execute(self) -> None:
-        thread = Thread(target=self.logic_thread)
+        thread = Thread(target=self.second_thread)
         thread.start()
 
-    def logic_thread(self) -> None:
-        self.view.status = "Running!"
-        email = self.view.email
-        password = self.view.password
-        api_key = self.view.api_key
+    def second_thread(self) -> None:
+        try:
+            self.normal_operation()
+        except ValueError:
+            self.view.status = "Ended Prematurely! Please check config.ini, and your email, and password is correct"
 
-        sleep(5)
-        print(email, password, api_key)
-        self.view.status = "Ready!"
+    def normal_operation(self) -> None:
+        self.view.status = "Downloading Data..."
+        self.model.fetch_data(
+            metabase_email=self.view.email, metabase_password=self.view.password
+        )
+
+        self.view.status = "Filtering orders.."
+        self.model.filter()
+
+        self.view.status = "Calculating Totals..."
+
+        self.view_status = "Ready!"
