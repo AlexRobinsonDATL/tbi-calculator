ef32abfd0aa0acc987cfcf948023e3fa9bae7877
AlexRobinsonDATL <alexander.robinson@dunlopatl.co.uk>
2021-04-23 11:31:44 +0100

    :bug: fix config not reading file

--- tbi_calculator/config.py
+++ tbi_calculator/config.py
@@ -4,23 +4,19 @@ import configparser
 from pathlib import Path
 
 default_config = configparser.ConfigParser()
-
 default_config["Smartsheet"] = {
     "API_KEY": "",
     "SHEET_ID": "8604312818476932",
     "COLUMN_NAME": "Syteline Account Code",
 }
-
 default_config["Metabase"] = {"URL": "", "QUERY_NUMBER": "82"}
 
-
 config = configparser.ConfigParser()
-
 file_path = Path(__file__).parents[1] / "config.ini"
 
 try:
-    with open(file_path, "r") as f:
-        config.read(f)
+    config.read(file_path)
 except FileNotFoundError:
     with open(file_path, "w") as f:
         default_config.write(f)
+    config = default_config
